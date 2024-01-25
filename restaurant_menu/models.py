a58ba2746ccaa7fbee4c43db57aabe5cbbca2f12
Deepa_Git <deepaghivari14@gmail.com>
2024-01-25 13:28:12 +0530

    Creating a Database Model

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,29 @@
-from django.db import models
+from django.db import models                                                     # models-library, Model-class
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes","Main Dishes"),
+    ("desserts", "Deserts")
+)
+
+STATUS = (
+    (0, "Unavailable"),
+    (1, "Available")
+)
+
+
+class Item(models.Model):                                                       # Item is class we inherit it from Model class of models library
+    meal = models.CharField(max_length=2000,unique=True)                         # unique-this will protect database from havin duplicate values(duplicate meals)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
+
