6091899380e1df198e210d6d8f1a8a5649271429
Deepa_Git <deepaghivari14@gmail.com>
2024-01-26 12:39:09 +0530

    Context in Django

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -7,6 +7,11 @@ class MenuList(generic.ListView):
     queryset = Item.object.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {"meals":["Pizza","Burger"],
+                   "starter":"gobi"}
+        return context
+
 
 
 
@@ -34,4 +39,7 @@ class MenuListDetail(generic.DetailView):
 # 2 types of views we can define in views.py
 # 1.function based views-which has more code
 # 2.class based views-which has less/clean code-use any of these two based on your project purpose
+
+# get_context_data(exact name) is predefined method of ListView class
+# context is a dictionary made up of keys and values and from html page we can access values by using keys of that context
 
