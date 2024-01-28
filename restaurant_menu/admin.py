5cb5aabc238a0b11bd6778aac44e438d066a4549
Deepa_Git <deepaghivari14@gmail.com>
2024-01-28 13:26:26 +0530

    Context in Django _Jinja "for" loops - {%  for row in item_list %} - here item_list represtens Item class in models.py and list of that Item class and row is eac row of model database/row of each of class Item in models.py

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+class MenuListAdmin(admin.ModelAdmin):
+    list_display = ("meal","status")
+    list_filter = ("status",)
+    search_fields = ("meal","description")
+
+
+admin.site.register(Item,MenuListAdmin)
