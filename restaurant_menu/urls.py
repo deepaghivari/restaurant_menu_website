6e5ab5e66abe99211b9502aa7a9dd51a2e083a7a
Deepa_Git <deepaghivari14@gmail.com>
2024-01-28 14:32:32 +0530

    Adding Dynamic Links -Adding Dynamic Links to each meal giving meal name,description and other details like price by clicking on link on each meal

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.MenuList.as_view(),name="home"),
-    path('',views.MenuListDetail.as_view(),name="menu_item_detail")
+    path('item/<int:pk>',views.MenuListDetail.as_view(),name="menu_item_detail") #to make url more dynamic,we can add this syntax ie 'item/<int:pk>' ,we will expects integer from primary key(pk) of database.because each row of databse has primary key attached to it # after adding '<int:pk>' to url part we got out of error on web page
 ]
 
 
