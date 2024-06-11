9a31f25f7351e0f337f5a577d96704efdc58c003
deepa_ghiwari <87002626+deepaghivari@users.noreply.github.com>
2024-06-11 19:41:43 +0530

    Update urls.py

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -8,6 +8,3 @@ urlpatterns = [
 
 
 
-# we need to call as_view() method to render the mentioned page when user visit the url however we dont need to call this metod in function based views(views.py)
-# ex of homepage url www.google.com/   so homepage has just '/'
-# ex of other page inside google page www.google.com/about
